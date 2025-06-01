6010c403e2c776448749fc06337415ba77c08e5e
projectkorea <prodopark15@gmail.com>
2025-06-01 21:44:07 +0900

    feat: update packages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 import tailwindcssPostcss from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   base: '/chat/',
   // base: "/static/chat/",
   css: {
@@ -23,19 +23,4 @@ export default defineConfig({
     emptyOutDir: true,
     cssCodeSplit: true,
   },
-  resolve: {
-    alias: {
-      'src': path.resolve(__dirname, './src'),
-      '@': path.resolve(__dirname, './src'),
-      'components': path.resolve(__dirname, './src/components'),
-      'types': path.resolve(__dirname, './src/types'),
-      'pages': path.resolve(__dirname, './src/pages'),
-      'hooks': path.resolve(__dirname, './src/hooks'),
-      'utils': path.resolve(__dirname, './src/utils'),
-      'store': path.resolve(__dirname, './src/store'),
-      'assets': path.resolve(__dirname, './src/assets'),
-      'routes': path.resolve(__dirname, './src/routes'),
-      'common': path.resolve(__dirname, './src/components/common')
-    }
-  }
 })
